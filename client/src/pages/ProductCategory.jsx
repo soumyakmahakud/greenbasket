e33dfa79c79dd023f1d6d37feb87b69a4ff068d9
soumyakant mahakud <150366334+soumyakmahakud@users.noreply.github.com>
2025-08-19 22:47:27 +0530

    Refactor grid layout in BestSeller, AllProducts, and ProductCategory components for consistency

--- client/src/pages/ProductCategory.jsx
+++ client/src/pages/ProductCategory.jsx
@@ -20,7 +20,7 @@ const ProductCategory = () => {
                 </div>
       )}
       {filteredProducts.length > 0 ? (
-        <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-3 md:gap-6 mt-6'>
+        <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-3 md:gap-3 lg:gap-2 lg:grid-cols-5 mt-6'>
             {filteredProducts.map((product)=>(
                 <ProductCard key={product._id} product={product}/>
             ))}
